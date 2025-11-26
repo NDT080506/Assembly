30ba52b7b11dcf0f69c258f3b9e0e5d0f8e0ef5c
DTX <agutokun.nii@gmail.com>
2025-11-26 21:53:10 +0700

    Add files via upload

---
+++ bai12.asm
@@ -0,0 +1,58 @@
+.model small
+.stack 100h
+.data
+    num db 10 dup('$')
+    tb db 10, 13, "Tong cac chu so la: $"
+    
+.code
+    main proc
+        mov ax, @data
+        mov ds, ax
+        
+        lea dx, num
+        mov ah, 10
+        int 21h
+        
+        
+        mov cl, [num+1]
+        lea si, num+2
+        mov ax, 0
+        
+        sum:
+            mov dx, 0
+            mov dl, [si]
+            sub dl, '0'
+            add ax, dx
+            inc si
+            dec cl
+            jnz sum
+            
+        
+        mov bl, 10
+        mov cl, 0
+        
+        convert_num:
+            mov ah, 0
+            div bl
+            push ax   
+            inc cl
+            cmp al, 0
+            jne convert_num
+            
+        lea dx, tb
+        mov ah, 9
+        int 21h      
+        
+        print_num:
+            pop ax
+            mov dl, ah
+            add dl, '0'
+            mov ah, 2
+            int 21h
+            dec cl
+            jnz print_num
+        
+        mov ah, 4ch
+        int 21h
+    main endp
+end         
